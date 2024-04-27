26c99764ce19572ffb20bf7d54e01727e82a11b0
susanharvey <susan-harvey6951@sbcglobal.net>
2024-04-26 21:18:44 -0600

    added comments, switched to a do while loop, and simplified int.Parse code.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,21 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Code for user picking magic number.
         // Console.Write("What is the magic number? ");
        // string magicString = Console.ReadLine();
        // int magicNumber = int.Parse(magicString);
         
+        // Call random generator and have it pick magic number
         Random randomGenerator = new Random();
         int magicNumber = randomGenerator.Next(1,11);
 
+        // loop for while user is guessing the magic number
         int guess = -1;
 
          while (magicNumber != guess)
         {
-            Console.Write("What is your guess? ");
+            Console.Write("What is your guess for the magic number? ");
             string userNumber = Console.ReadLine();
             guess = int.Parse(userNumber);
 
+            // if/else statements for higher/lower/you guessed it displays.
             if (guess > magicNumber)
             {
                 Console.WriteLine("lower");
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,11);
+
+        // do while loop for user guessing the magic number.
+        int guess = -1;
+
+        do
+        {
+            Console.Write("What is your guess for the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            // if/else statements for higher/lower/you guessed it displays.
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                 Console.WriteLine("You guessed it!");
+            } 
+        } while (magicNumber != guess);
     }
 }
