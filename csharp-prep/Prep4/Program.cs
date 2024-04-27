4dd46b3636de741494a2c26460cfa32f4cb3f20d
susanharvey <susan-harvey6951@sbcglobal.net>
2024-04-26 23:07:33 -0600

    added comments, coded do while loop for creating list, and used sort to find the max number

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,49 +5,55 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Create new list called numbers.
         List<int> numbers = new List<int>();
        
         // Display User Instructions
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
 
         int number = -1;
-        while (number != 0)
+        
+        /*do while loop and if statement to get user numbers for list,
+        add them to the numbers list numbers until user enters 0*/
+        do
         {
             Console.Write("Enter Number: ");
-            string response = Console.ReadLine();
-            number = int.Parse(response);
+            number = int.Parse(Console.ReadLine());
 
             if (number != 0)
             {
                 numbers.Add(number);
             }
 
-        }
+        } while (number != 0);
+
+        // Foreach loop to add together all the digits in the numbers list.
         int sum = 0;
         foreach(int digit in numbers)
         {
             sum += digit;
         }
+
+        // Display the sum
         Console.WriteLine($"The sum is {sum}");
         
+        //Find the average of the numbers in the list.
         float average;
         average =  (float)sum / numbers.Count;
+
+        //Display the numbers list average.
         Console.WriteLine($"The average is: {average}");
 
-
-        int maxNum = numbers[0];
-        foreach (int i in numbers)
-        {
-            if (i > maxNum)
-            {
-                maxNum = i;
-            }
-        }
-        
-        Console.WriteLine($"The max is: {maxNum}");
-
-        
+       //Sort the numbers list from low to high.
         numbers.Sort();
+
+        //Find the largest number in the list.
+        int maxNum = numbers.Last();
+
+        //Display the largest number.
+        Console.WriteLine($"The largest number is: {maxNum}");
+        
+        // Foreach loop to display the sorted list.
         Console.WriteLine("The sorted list is:");
         foreach (int digit in numbers)
         {
