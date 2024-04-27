a7a65919f94441694368bf1f4ac2ceefb9e8bc16
susanharvey <susan-harvey6951@sbcglobal.net>
2024-04-26 23:23:47 -0600

    Restore Sandbox to initial commit.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,30 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1,11);
-
-        // do while loop for user guessing the magic number.
-        int guess = -1;
-
-        do
-        {
-            Console.Write("What is your guess for the magic number? ");
-            guess = int.Parse(Console.ReadLine());
-
-            // if/else statements for higher/lower/you guessed it displays.
-            if (guess > magicNumber)
-            {
-                Console.WriteLine("lower");
-            }
-            else if (guess < magicNumber)
-            {
-                Console.WriteLine("Higher");
-            }
-            else
-            {
-                 Console.WriteLine("You guessed it!");
-            } 
-        } while (magicNumber != guess);
+       Console.WriteLine("Welcome to Sandbox World!");
     }
 }
