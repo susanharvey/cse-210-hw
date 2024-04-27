8fbdc978b05c389a6550fd42cab3e26aca479253
susanharvey <susan-harvey6951@sbcglobal.net>
2024-04-26 21:25:57 -0600

    added comments, switch from while loop to do while loop, simplified int. Parse code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,10 +1,12 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
         // Code for user picking magic number.
+
         // Console.Write("What is the magic number? ");
        // string magicString = Console.ReadLine();
        // int magicNumber = int.Parse(magicString);
@@ -12,15 +14,14 @@ class Program
         // Call random generator and have it pick magic number
         Random randomGenerator = new Random();
         int magicNumber = randomGenerator.Next(1,11);
-
-        // loop for while user is guessing the magic number
+        
         int guess = -1;
-
-         while (magicNumber != guess)
-        {
+         // Do while loop for user guessing the magic number.
+        do
+        { 
+            // Let user guess magic number and change string response to int.
             Console.Write("What is your guess for the magic number? ");
-            string userNumber = Console.ReadLine();
-            guess = int.Parse(userNumber);
+            guess = int.Parse(Console.ReadLine());
 
             // if/else statements for higher/lower/you guessed it displays.
             if (guess > magicNumber)
@@ -34,8 +35,8 @@ class Program
             else
             {
                  Console.WriteLine("You guessed it!");
-            }
-        }
+            } 
+        } while (magicNumber != guess);
 
     }
 }
