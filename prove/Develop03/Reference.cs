f96e4c6330d4d986e5e91333b2fc813f1d5ae58e
susanharvey <susan-harvey6951@sbcglobal.net>
2024-05-07 04:25:30 -0600

    Added Scripture, Reference, and Word cs files. Stubbed those 3 files.

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Reference
+{
+   private string _book;
+   private int _chapter;
+   private int _verse;
+   private int _endVerse;
+
+   public Reference(string book, int chapter, int verse)
+   {
+
+   }
+   public Reference(string book, int chapter, int verse, int end_verse)
+   {
+
+   }
+
+   public string GetDisplayText()
+   {
+    return "";
+   }
+   
+
+    
+}
