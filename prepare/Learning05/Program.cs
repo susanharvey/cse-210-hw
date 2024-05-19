4192c9b4d669d9349bec533621f5d6d72fc21692
susanharvey <susan-harvey6951@sbcglobal.net>
2024-05-18 19:47:02 -0600

    I added the code for learning #5, Shapes.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new();
+
+        Square square = new("purple", 16);
+        shapes.Add(square);
+
+        Rectangle rectangle = new("blue", 3, 10);
+        shapes.Add(rectangle);
+
+        Circle circle = new("yellow", 20);
+        shapes.Add(circle);
+       
+       foreach(Shape s in shapes)
+       {
+        string color = s.GetColor();
+        double area = s.GetArea();
+        Console.WriteLine($"The {color} shape has an area of {area}.");
+       }
     }
 }
