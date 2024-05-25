b25b0cb738f28321a45adc08504a23320b6b3134
susanharvey <susan-harvey6951@sbcglobal.net>
2024-05-25 10:37:14 -0700

    Added source for congrats messages

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -3,6 +3,8 @@ using System;
 //Creativity/enhancement: I added some user input guards, a guard and message for if the user
 //records a goal already completed, congrats messages for recording each type of goal,
 //and special congrats messages for reaching 5000, or 10000 points.
+
+//Congrats Message Source: https://www.americangreetings.com/inspiration/what-to-write/congratulations-messages
 class Program
 {
     static void Main(string[] args)
